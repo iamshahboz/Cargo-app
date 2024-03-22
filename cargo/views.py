bc5135a4caa7e6db392cd76c785e2955ea162535
Shahboz <sharafzodashahboz@gmail.com>
2024-03-22 06:54:20 +0500

    import from serializers and models

--- cargo/views.py
+++ cargo/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from .serializers import CargoSerializer, CarSerializer, LocationSerializer
+from .models import Car, Cargo, Location
 
 def homepage(request):
     return HttpResponse('<center>This is the Cargo application</center>')
