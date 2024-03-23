5fcfacf177caae1b353a0a0fd6494e178e84f4d6
Shahboz <sharafzodashahboz@gmail.com>
2024-03-23 07:07:44 +0500

    urls configured

--- cargo/urls.py
+++ cargo/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.homepage, name='homepage'),
+    path('cargo/<int:pk>/',views.CargoEdit.as_view()),
+    path('car/<int:pk>/',views.CarEdit.as_view()),
 
 ]
