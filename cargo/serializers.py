76b88e82a01045ef0914a66a49c142a69f0bffc0
Shahboz <sharafzodashahboz@gmail.com>
2024-03-23 07:07:04 +0500

    serializers created

--- cargo/serializers.py
+++ cargo/serializers.py
@@ -5,18 +5,18 @@ class CargoSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Cargo
-        fields = ['id','pick_up_longitude','pick_up_latitude',
-                  'delivery_longitude','delivery_latitude','weight','description']
+        fields = ['id','pick_up_location','delivery_location',
+                  'weight','description']
         
 class CarSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Car 
-        fields = ['id','plate_number','current_location_longitude','current_location_latitude','carrying_capacity']
+        fields = ['id','plate_number','current_location','capacity']
 
 class LocationSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     class Meta:
         model = Location
-        fields = ['id','city','state','zip_code','longitude','latitude']
+        fields = ['id','city','state','zip_code','latitude','longitude']
         
