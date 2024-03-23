044fbd1a1f4f8bc9b4683567e52a7254f5fe5d4f
Shahboz <sharafzodashahboz@gmail.com>
2024-03-23 07:07:22 +0500

    edit views done

--- cargo/views.py
+++ cargo/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from .serializers import CargoSerializer, CarSerializer, LocationSerializer
 from .models import Car, Cargo, Location
+from rest_framework import generics
 
 def homepage(request):
     return HttpResponse('<center>This is the Cargo application</center>')
+
+
+class CargoEdit(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Cargo.objects.all().order_by('-id')
+    serializer_class = CargoSerializer
+
+class CarEdit(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Car 
+    serializer_class = CarSerializer
+    
 
