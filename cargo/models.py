2436a67260ef83228b8909782fea0a9085ec6598
Shahboz <sharafzodashahboz@gmail.com>
2024-03-23 06:20:58 +0500

    models got changed

--- cargo/models.py
+++ cargo/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class Location(models.Model):
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.IntegerField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    
 
 class Cargo(models.Model):
-    pick_up_longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    pick_up_latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    delivery_longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    delivery_latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    pick_up_location = models.ForeignKey(Location, related_name='pick_up_cargos',on_delete=models.CASCADE)
+    delivery_location = models.ForeignKey(Location,related_name='delivery_cargos',on_delete=models.CASCADE)
     weight = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
     description = models.TextField()
 
@@ -14,20 +19,14 @@ class Cargo(models.Model):
         return self.weight
 
 class Car(models.Model):
-    plate_number = models.CharField(max_length=5)
-    current_location_longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    current_location_latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    carrying_capacity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
+    plate_number = models.CharField(max_length=5, unique=True)
+    current_location = models.ForeignKey(Location, related_name='car_location', on_delete=models.CASCADE)
+    capacity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
 
     def __str__(self):
         return self.plate_number
     
-class Location(models.Model):
-    city = models.CharField(max_length=100)
-    state = models.CharField(max_length=100)
-    zip_code = models.IntegerField()
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+
 
 
 
