9abbf16737120813971336c8bb01b35a81b228d4
Shahboz <sharafzodashahboz@gmail.com>
2024-03-24 06:56:18 +0500

    url for swagger config added

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,26 @@
-
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    validators=['ssv', 'flex'],
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('cargo.urls')),
+    path('swagger/',schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
 ]
