fcf42d0c0728d966c9aab81fbee65c8269cb03a0
Shahboz <sharafzodashahboz@gmail.com>
2024-03-24 06:47:58 +0500

    return methods changed

--- cargo/models.py
+++ cargo/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Location(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.IntegerField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.zip_code
     
 
 class Cargo(models.Model):
@@ -18,13 +22,15 @@ class Cargo(models.Model):
     def __str__(self):
         return self.weight
 
+
 class Car(models.Model):
     plate_number = models.CharField(max_length=5, unique=True)
     current_location = models.ForeignKey(Location, related_name='car_location', on_delete=models.CASCADE)
     capacity = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(1000)])
 
-    def __str__(self):
-        return self.plate_number
+    # def __str__(self):
+    #     return self.capacity
+    
     
 
 
